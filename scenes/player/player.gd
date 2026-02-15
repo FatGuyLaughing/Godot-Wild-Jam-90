35a934d4d7d3df53f41cd3d790774514d8cbfff0
fatguylaughing <fatguylaughing@gmail.com>
2026-02-14 18:25:26 -0600

    Adds basic player state machine
    Adds a state machine to handle player state transitions.
    
    Transitions to "Hurt" state upon taking damage and "Dead" state upon death.
    
    Introduces an Idle state.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,27 +7,22 @@ extends CharacterBody2D
 ## should connect to this to handle game over.
 signal died()
 
-@onready var _animation_player: AnimationPlayer = %AnimationPlayer
+
 @onready var _hurtbox: HurtboxComponent = %HurtboxComponent
 @onready var _hitbox: HitboxComponent = %HitboxComponent
 @onready var _health: HealthComponent = %HealthComponent
+@onready var _state_machine: StateMachine = %StateMachine
 
 
 func _ready() -> void:
+	_hurtbox.hit.connect(_on_hurtbox_hit)
 	_health.died.connect(_on_died)
 	_hitbox.enabled = false
 
 
 func _on_died() -> void:
-	_hurtbox.enabled = false
-	_hitbox.enabled = false
-	died.emit()
+	_state_machine.transition_to("Dead")
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack"):
-		_attack()
-
-
-func _attack() -> void:
-	_animation_player.play("attack")
+func _on_hurtbox_hit(_incoming_hitbox: HitboxComponent, _damage: float) -> void:
+	_state_machine.transition_to("Hurt")
---
+++ scenes/player/states/idle.gd
@@ -0,0 +1,22 @@
+## Player idle state. Active when no movement input is detected.
+## Transitions to Move on directional input and Attack on attack input.
+class_name PlayerIdleState
+extends State
+
+@onready var _animation_player: AnimationPlayer = %AnimationPlayer
+
+
+func enter() -> void:
+	_animation_player.play("idle")
+
+
+func physics_process(_delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	if direction != Vector2.ZERO:
+		transition_to("Move")
+		return
+
+	if Input.is_action_just_pressed("attack"):
+		transition_to("Attack")
+		return
