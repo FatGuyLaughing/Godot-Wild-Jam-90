cdc1b23dfaae27db769c5a6d131d8a0c109fd506
hatmix <67128187+hatmix@users.noreply.github.com>
2026-02-17 13:56:46 -0500

    Add tileset art and update build room similar to art demo

--- scenes/game_scene/game.gd
+++ scenes/game_scene/game.gd
@@ -2,11 +2,9 @@ extends Node2D
 ## This is WIP initial gameplay and scene structure will probably change.
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
