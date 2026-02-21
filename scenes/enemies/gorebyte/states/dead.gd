4b3902bd72d04dcd6dc453548f682bbfdba49465
fatguylaughing <fatguylaughing@gmail.com>
2026-02-21 16:10:39 -0600

    Gorebyte death spawns three gorebits

--- scenes/enemies/gorebyte/states/dead.gd
+++ scenes/enemies/gorebyte/states/dead.gd
@@ -1,6 +1,10 @@
 class_name EnemyGorebyteDeadState
 extends State
 
+const GOREBIT_SCENE: PackedScene = preload("res://scenes/enemies/gorebit/gorebit.tscn")
+
+@export var spawn_offset: float = 20.0
+
 var gorebyte: GorebyteEnemy
 
 @onready var _animation_player: AnimationPlayer = %AnimationPlayer
@@ -26,4 +30,19 @@ func physics_process(_delta: float) -> void:
 
 func _on_animation_finished(animation_name: String) -> void:
 	if animation_name == "dead":
+		_spawn_gorebits()
 		gorebyte.queue_free()
+
+
+func _spawn_gorebits() -> void:
+	var parent = gorebyte.get_parent()
+	var offsets: Array[Vector2] = [
+		Vector2(0, -1) * spawn_offset,
+		Vector2(-0.866, 0.5) * spawn_offset,
+		Vector2(0.866, 0.5) * spawn_offset,
+	]
+
+	for offset in offsets:
+		var gorebit = GOREBIT_SCENE.instantiate()
+		gorebit.global_position = gorebyte.global_position + offset
+		parent.call_deferred("add_child", gorebit)
