70b39db0a01b73a3ce3381b4a252d99bb987103a
fatguylaughing <fatguylaughing@gmail.com>
2026-02-21 20:38:57 -0600

    Removed _ready() from Vargazer.

--- scenes/enemies/vargazer/vargazer.gd
+++ scenes/enemies/vargazer/vargazer.gd
@@ -4,7 +4,3 @@ extends EnemyBase
 @onready var laser_origin: Marker2D = %LaserOrigin
 @onready var label: Label = $Label
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-signal enemy_died
-
-func _ready() -> void:
-	add_to_group("enemies")
