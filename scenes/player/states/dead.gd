85eaddba1869e3218b171bb99001d89a10b37713
fatguylaughing <fatguylaughing@gmail.com>
2026-02-22 14:20:11 -0600

    Changes to credits on player death
    Implements a navigation to the credits scene after the player character dies.
    
    After the player character is removed from the game, a timer is triggered which will cause a scene change to the credits.

--- scenes/player/states/dead.gd
+++ scenes/player/states/dead.gd
@@ -4,8 +4,15 @@
 class_name PlayerDeadState
 extends State
 
+const CREDITS_SCENE_PATH: String = "res://scenes/credits/scrolling_credits.tscn"
+
 @onready var _animation_player: AnimationPlayer = %AnimationPlayer
 
 
 func enter() -> void:
 	entity.queue_free()
+	get_tree().create_timer(1.0).timeout.connect(_go_to_credits)
+
+
+func _go_to_credits() -> void:
+	get_tree().change_scene_to_file(CREDITS_SCENE_PATH)
